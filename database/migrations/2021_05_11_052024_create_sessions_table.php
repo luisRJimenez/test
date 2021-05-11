7f31656f0131969fae1177d1175234610cffc55a
luisRJimenez <luisrodrigojimenez@gmail.com>
2021-05-11 01:55:48 -0500

    correcion definiiva

--- database/migrations/2021_05_11_052024_create_sessions_table.php
+++ database/migrations/2021_05_11_052024_create_sessions_table.php
@@ -13,8 +13,9 @@ class CreateSessionsTable extends Migration
      */
     public function up()
     {
+        \Illuminate\Support\Facades\DB::statement('SET SESSION sql_require_primary_key=0');
         Schema::create('sessions', function (Blueprint $table) {
-            $table->id();
+            $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
